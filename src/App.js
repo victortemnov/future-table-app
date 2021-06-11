d23905e39dc1d5e3cba2dcaf83e861720bfb45ca
ekke <victortemnov@gmail.com>
2021-06-11 15:08:49 +0300

    get data from link after DOM render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const response = await fetch(
+      `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+    );
+    const data = await response.json();
+  }
+
   render() {
-    <div className="container">table</div>;
+    return <div className="container">table</div>;
   }
 }
 
