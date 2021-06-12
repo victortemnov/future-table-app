9d15493882cf6e1f0319a0f09c3a0d506d8c9220
ekke <victortemnov@gmail.com>
2021-06-12 17:33:37 +0300

    customize main page

--- src/components/ModeSelector/ModeSelector.js
+++ src/components/ModeSelector/ModeSelector.js
@@ -4,28 +4,24 @@ const ModeSelector = (props) => {
   const smallUrl = `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
   const bigUrl = `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "300px",
-      }}
-    >
-      <h3 style={{ marginRight: "30px" }}>Сколько контактов отобразить? </h3>
-      <button
-        style={{ marginRight: "50px", width: "100px" }}
-        onClick={() => props.onSelect(smallUrl)}
-        className="btn btn-outline-primary"
-      >
-        32
-      </button>
-      <button
-        style={{ width: "100px" }}
-        onClick={() => props.onSelect(bigUrl)}
-        className="btn btn-outline-success"
-      >
-        1000
-      </button>
+    <div className="d-flex align-items-center flex-column">
+      <h3>Выберите кол-во контактов</h3>
+      <div>
+        <button
+          style={{ width: "100px", marginRight: "30px" }}
+          onClick={() => props.onSelect(smallUrl)}
+          className="btn btn-outline-primary"
+        >
+          32
+        </button>
+        <button
+          style={{ width: "100px" }}
+          onClick={() => props.onSelect(bigUrl)}
+          className="btn btn-outline-success"
+        >
+          1000
+        </button>
+      </div>
     </div>
   );
 };
